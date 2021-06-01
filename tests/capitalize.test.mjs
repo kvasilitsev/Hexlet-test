19162120f227fb482bef8ca5f9a1ff9c41aebf75
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-05-31 22:04:22 -0300

    Capitalize function and test

---
+++ tests/capitalize.test.mjs
@@ -0,0 +1,10 @@
+import capitalize from '../src/capitalize.mjs';
+if (capitalize('hello') !== 'Hello') {
+  throw new Error('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+  throw new Error('Функция работает неверно!');
+}
+
+console.log('Все тесты пройдены!');
