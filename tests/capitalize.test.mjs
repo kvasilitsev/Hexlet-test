98c296521533fc17690b1626539b0e4142404bb1
Konstantin Vasilitsev <kvasylitsev@gmail.com>
2021-05-31 22:13:09 -0300

    Change IF to ASSERT

--- tests/capitalize.test.mjs
+++ tests/capitalize.test.mjs
@@ -1,10 +1,6 @@
+import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.mjs';
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
